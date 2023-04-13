948e652f1c5454fa5bfbc055be75852878be9fd3
wojcikowska-k <116753173+wojcikowska-k@users.noreply.github.com>
2023-04-13 10:51:43 +0000

    Deploying to gh-pages from @ wojcikowska-k/goit-js-hw-09@8ee17b14b88a502523b0bbe5c64a55601478f00f 🚀

---
+++ 01-color-switcher.52e816d2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),a=null;t.addEventListener("click",(function(){a=setInterval((function(){n.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.52e816d2.js.map

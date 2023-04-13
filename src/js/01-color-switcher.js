8ee17b14b88a502523b0bbe5c64a55601478f00f
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-04-13 12:52:06 +0200

    task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let timerId = null;
+
+startBtnEl.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+});
+
+stopBtnEl.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
